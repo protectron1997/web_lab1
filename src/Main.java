72e2fae01058849b9a7e764dfb8bce5f00d7d637
tom <tom@>
2024-10-08 13:09:39 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.nio.charset.StandardCharsets;
+
+import com.fastcgi.FCGIInterface;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        var fcgiInterface = new FCGIInterface();
+        while (fcgiInterface.FCGIaccept() >= 0) {
+            var content = """
+                    <html>
+                        <head><title>Java FastCGI Hello World</title></head>
+                        <body><h1>Hello, World!</h1></body>
+                    </html>""";
+            var httpResponse = """
+                    HTTP/1.1 200 OK
+                    Content-Type: text/html
+                    Content-Length: %d
+                    
+                    %s
+                    """.formatted(content.getBytes(StandardCharsets.UTF_8).length, content);
+            System.out.println(httpResponse);
         }
     }
 }
