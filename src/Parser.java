b975a6794ac6a50050dee815d4c0203ef1d25476
tom <tom@>
2024-10-29 16:14:16 +0300

    complete parse method for class Parsers

--- src/Parser.java
+++ src/Parser.java
@@ -12,9 +12,11 @@ public class Parser{
 
 
 
-    public static void parse (String data) throws ParseExeption {
+    public HashMap<String, String> parse (String data) throws ParseExeption {
         double x,r;
         int y;
+        boolean badFlag = false;
+        StringBuilder badMessage = new StringBuilder();
         HashMap<String, String> extValues = new HashMap<>();
 
 
@@ -27,7 +29,8 @@ public class Parser{
                 if (x >= -5 && x <= 3) {
                     this.x = x;
                 } else {
-                    throw new ParseExeption("false x");
+                    badFlag = true;
+                    badMessage.append("false x");
                 }
             }
             if (Objects.equals(keyValue[0], "y")) {
@@ -35,7 +38,8 @@ public class Parser{
                 if (y >= -4 && y <= 4) {
                     this.y = y;
                 } else {
-                    throw new ParseExeption("false y");
+                    badFlag = true;
+                    badMessage.append("false y");
                 }
             }
             if (Objects.equals(keyValue[0], "r")) {
@@ -43,11 +47,22 @@ public class Parser{
                 if (r >= 2 && r <= 5) {
                     this.r = r;
                 } else {
-                    throw new ParseExeption("false r");
+                    badFlag = true;
+                    badMessage.append("false r");
                 }
             }
-        }
+            if (badFlag){
+                throw new ParseExeption(badMessage.toString());
+            }
+            else{
+                extValues.put("x", String.valueOf(this.x));
+                extValues.put("y", String.valueOf(this.x));
+                extValues.put("r", String.valueOf(this.x));
+                return extValues;
+            }
+
 
     }
+        return extValues;
     }
 }
