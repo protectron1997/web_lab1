6459e0ed0561d9421ce174ff0926a8065d7bc36a
tom <tom@>
2024-11-04 18:26:30 +0300

    final changes

--- src/share/JSON.java
+++ src/share/JSON.java
@@ -1,15 +1,15 @@
 package share;
 
 public class JSON {
-    private static String json = """
-            {
-            "result": "%s",
-            "time_exec": "%s",
-            "date": "%s"
-            }
-            """;
 
     public static String genStandardJSON(String result, String timeExec, String date){
+        String json = """
+                {
+                "result": "%s",
+                "time_exec": "%s",
+                "date": "%s"
+                }
+                """;
         return json.formatted(result,timeExec,date);
     }
 }
